ed6496745061d5e6d2ec8d0aa01e22c946d8b8bf
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-07 21:39:57 +0530

    Add user table

--- stockyeh/backend/models/user.py
+++ stockyeh/backend/models/user.py
@@ -1,10 +1,9 @@
-from sqlalchemy.orm import DeclarativeBase
+from stockyeh.backend.models.model import MappedBase
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
 
 
-class User(DeclarativeBase):
-    __tablename__ = "users"
+class User(MappedBase):
 
     id: Column[int] = Column(Integer, primary_key=True, index=True)
     username: Column[str] = Column(String(20), unique=True, index=True, nullable=False)
@@ -12,12 +11,11 @@ class User(DeclarativeBase):
     joined_at: Column = Column(DateTime(timezone=True), server_default=func.now())
 
 
-class Portfolio(DeclarativeBase):
-    __tablename__ = "portfolios"
+class Portfolio(MappedBase):
 
     user_id: Column[int] = Column(
         Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
-    asset_id: Column[int] = Column(Integer, ForeignKey("assets.id", ondelete="CASCADE"))
+    asset_id: Column[int] = Column(Integer, ForeignKey("asset.id", ondelete="CASCADE"))
     amount: Column[int] = Column(Integer, nullable=False)
     bought_at: Column = Column(DateTime(timezone=True), server_default=func.now())
