e40abe9b679d102e4aaf550848f68936fd00a6b4
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-08 17:35:04 +0530

    Setup database creation

--- stockyeh/backend/database.py
+++ stockyeh/backend/database.py
@@ -1,21 +1,32 @@
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
+from typing import Annotated
 
 from fastapi import Depends
 from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from typing_extensions import Annotated
+from sqlalchemy.orm import declarative_base
 from stockyeh.backend.utils.config import env
-from stockyeh.backend.models.model import MappedBase
 
 
-def create_engine_and_session(url: str | URL):
+class DatabaseError(Exception):
+    """Error raised due to unhandeled database exception."""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+def create_engine_and_session(url: str | URL) -> tuple:
+    """Create an async engine and session."""
     try:
         engine = create_async_engine(url, future=True, pool_pre_ping=True)
     except Exception as e:
-        raise e
+        raise DatabaseError(e) from e
     else:
         db_session = async_sessionmaker(
-            bind=engine, autoflush=False, expire_on_commit=False
+            bind=engine,
+            autoflush=False,
+            expire_on_commit=False,
+            class_=AsyncSession,
         )
         return engine, db_session
 
@@ -24,14 +35,17 @@ SQLALCHEMY_DATABASE_URL = env.DATABASE_URL
 
 async_engine, async_db_session = create_engine_and_session(SQLALCHEMY_DATABASE_URL)
 
+Base = declarative_base()
+
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    """Get an async database session."""
     session: AsyncSession = async_db_session()
     try:
         yield session
     except Exception as se:
         await session.rollback()
-        raise se
+        raise DatabaseError(se) from se
     finally:
         await session.close()
 
@@ -39,7 +53,7 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
 CurrentSession = Annotated[AsyncSession, Depends(get_db)]
 
 
-async def create_tables():
+async def create_tables() -> None:
+    """Create database tables."""
     async with async_engine.begin() as conn:
-        e = await conn.run_sync(MappedBase.metadata.create_all)
-        print(e)
+        await conn.run_sync(Base.metadata.create_all)
--- stockyeh/backend/main.py
+++ stockyeh/backend/main.py
@@ -1,22 +1,18 @@
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
+from stockyeh.backend.database import create_tables
 from stockyeh.backend.utils.config import env
-from stockyeh.backend.database import CurrentSession, create_tables
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI) -> AsyncGenerator:  # noqa: ARG001
+    """Context manager for application lifespan."""
     await create_tables()
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(SessionMiddleware, secret_key=env.AUTH_SECRET)
-
-
-@app.get("/")
-async def root(db: CurrentSession):
-    print(db)
-    return {"message": "Hello World"}
