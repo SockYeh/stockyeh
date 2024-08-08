0e5856074dd8a172f407fb31dc2234d58e19c57d
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-08 22:34:15 +0530

    Add SMTP support for magic link

--- stockyeh/backend/utils/magic_link.py
+++ stockyeh/backend/utils/magic_link.py
@@ -1,20 +1,31 @@
 import time
+from email.mime.text import MIMEText
 from secrets import token_hex
 
+import aiosmtplib
 import jwt
 from config import env
 
-
-class MagicLinkExpiredError(Exception):
-    """Magic link expired error."""
-
-
-class MagicLinkIPMismatchError(Exception):
-    """Magic link ip mismatch error."""
+SMTP_EMAIL = env.SMTP_EMAIL
+SMTP_PASSWORD = env.SMTP_PASSWORD
+SMTP_HOST = env.SMTP_HOST
+SMTP_PORT = env.SMTP_PORT
 
 
-class MagicLinkInvalidError(Exception):
-    """Magic link invalid error."""
+class MLErrors:
+    """Errors for magic link."""
+
+    class ExpiredError(Exception):
+        """Magic link expired error."""
+
+    class IPMismatchError(Exception):
+        """Magic link ip mismatch error."""
+
+    class InvalidError(Exception):
+        """Magic link invalid error."""
+
+    class EmailError(Exception):
+        """Magic link email error."""
 
 
 def encode_magiclink_jwt(email: str, ip: str) -> str:
@@ -33,14 +44,36 @@ def decode_magiclink_jwt(token: str, ip: str) -> dict:
     try:
         decoded = jwt.decode(token, env.AUTH_SECRET, algorithms=["HS256"])
     except jwt.InvalidTokenError as e:
-        raise MagicLinkExpiredError from e
+        raise MLErrors.ExpiredError from e
 
     if decoded["ip"] != ip:
-        raise MagicLinkIPMismatchError
+        raise MLErrors.IPMismatchError
+
     if decoded["expires"] < time.time():
-        raise MagicLinkExpiredError
+        raise MLErrors.ExpiredError
 
     del decoded["random_gibberish"]
     del decoded["ip"]
 
     return decoded
+
+
+async def send_verification_email(email: str, token: str, url: str) -> bool:
+    """Send verification email."""
+    email_body = f'<pre> <a href="{url}?token={token}">click here</a> </pre>'
+
+    msg = MIMEText(email_body, "html")
+    msg["Subject"] = "StockYeh Magic link"
+    msg["From"] = SMTP_EMAIL
+    msg["To"] = email
+
+    try:
+        async with aiosmtplib.SMTP(SMTP_HOST, SMTP_PORT) as smtp:
+            await smtp.ehlo()
+            await smtp.login(SMTP_EMAIL, SMTP_PASSWORD)
+            await smtp.send_message(msg)
+            await smtp.quit()
+
+            return True
+    except aiosmtplib.errors.SMTPResponseException as e:
+        raise MLErrors.EmailError from e
