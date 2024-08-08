78fa580d3d09aa611a0e2960236f944d07c1af6b
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-08 22:34:15 +0530

    Add SMTP support for magic link

--- stockyeh/backend/utils/config.py
+++ stockyeh/backend/utils/config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = Field()
     AUTH_SECRET: str = Field()
+    SMTP_EMAIL: str = Field()
+    SMTP_PASSWORD: str = Field()
+    SMTP_HOST: str = Field()
+    SMTP_PORT: int = Field()
 
     model_config = SettingsConfigDict(env_file=".env")
 
