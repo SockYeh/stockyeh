4eb5d679476d6173de0d1f582477aa0de3087194
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-08 22:46:01 +0530

    Remove User schema

--- stockyeh/backend/schemas/auth.py
+++ stockyeh/backend/schemas/auth.py
@@ -1,15 +1,4 @@
-from datetime import datetime
-
 from pydantic import BaseModel
-
-
-class User(BaseModel):
-    """Schema for user."""
-
-    id: int
-    username: str
-    email: str
-    joined_at: datetime
 
 
 class UserLogin(BaseModel):
