7251cd86ecae7e838047d630f0aadb9d05600b5b
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-08 21:56:34 +0530

    Create magic link jwt handler

---
+++ stockyeh/backend/utils/magic_link.py
@@ -0,0 +1,39 @@
+import time
+from secrets import token_hex
+
+import jwt
+from config import env
+
+
+class MagicLinkExpiredError(Exception):
+    """Magic link expired error."""
+
+
+class MagicLinkIPMismatchError(Exception):
+    """Magic link ip mismatch error."""
+
+
+def encode_magiclink_jwt(email: str, ip: str) -> str:
+    """Encode magic link jwt."""
+    payload = {
+        "email": email,
+        "ip": ip,
+        "expires": time.time() + 60 * 60,
+        "random_gibberish": token_hex(32),
+    }
+    return jwt.encode(payload, env.AUTH_SECRET, algorithm="HS256")
+
+
+def decode_magiclink_jwt(token: str, ip: str) -> dict:
+    """Decode magic link jwt."""
+    decoded = jwt.decode(token, env.AUTH_SECRET, algorithms=["HS256"])
+
+    if decoded["ip"] != ip:
+        raise MagicLinkIPMismatchError
+    if decoded["expires"] < time.time():
+        raise MagicLinkExpiredError
+
+    del decoded["random_gibberish"]
+    del decoded["ip"]
+
+    return decoded
