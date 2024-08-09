e1581e7111f3dc91f7a4f809436f13ed622b6f70
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-08-09 23:49:42 +0530

    Move main.py

--- stockyeh/main.py
+++ stockyeh/main.py
@@ -3,9 +3,11 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
+
 from stockyeh.backend.database import create_tables
 from stockyeh.backend.routers import auth
 from stockyeh.backend.utils.config import env
+from stockyeh.frontend import frontend
 
 
 @asynccontextmanager
@@ -18,6 +20,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator:  # noqa: ARG001
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(SessionMiddleware, secret_key=env.AUTH_SECRET)
 
-routers = [auth.router]
+
+routers = [auth.router, frontend.router]
 for router in routers:
     app.include_router(router)
