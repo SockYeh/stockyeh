e1b815aff6fc1d1dd205dc31dfad46be3e6efae3
Nabeel <nabeelpasha74@gmail.com>
2026-05-10 22:10:04 +0530

    login error handeling added

--- stockyeh/frontend/src/login_page/login.tsx
+++ stockyeh/frontend/src/login_page/login.tsx
@@ -1,33 +1,38 @@
+import type { ReactDOMServerReadableStream } from 'react-dom/server';
 import background from '../backgrounds/Desktop - 8 (1).png';
-
-
-
-
 import image from '../backgrounds/Movie Inspired Poster_ ‘Moving Parts’.jpeg';
+import { useState, type ReactElement, type ReactHTMLElement } from 'react';
+
 
 
 function Login() {
+    const [message,setMessage]=useState("");
+
+    function handleSubmit(){
+        
+        if(!message.trim()){
+            alert("please enter");
+        }
+    }
+
+    const handleChange = (event) => {
+
+        setMessage(event.target.value);
+        
+    }
     return (
-
         <>
-
-            <img src={background} alt="background" className='w-400 h-182 z-0' />
-            
-
+            <img src={background} alt="background" className='w-400 h-182 z-0' /> 
             <div className=" h-140 w-250 flex    justify-between backdrop-blur-lg backdrop-brightness-90 shadow-2xl shadow-gray-400  position absolute top-25 left-65 rounded-3xl"> 
-                <img src={image} className='flex-1  rounded-3xl' />
-
-                
+                <img src={image} className='flex-1  rounded-3xl' />   
                 <div className='flex-2 '>
-                    <label>
-                        <input placeholder='Enter Sock ID'></input>
-                        <input placeholder='Enter PassKey'></input>
-                        <button>Login</button>
-
+                    <label className='grid grid-col-1 grid-rows-2 gap-40 justify-center '>
+                    
+                        <input placeholder='Enter PassKey' value={message} onChange={handleChange}  className='col-span-1 row-span-1'></input>
+                        <button className='col-span-1 row-span-1 border-2 rounded-lg' onClick={handleSubmit} >Login</button>
                     </label>
                 </div>
             </div>
-
         </>
     );
 }
